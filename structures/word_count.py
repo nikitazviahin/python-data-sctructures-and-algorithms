37275e3a7fd2788a921ee0a5013ac6ad58a3e906
nikitazviahin <mykytazviahin@yahoo.com>
2021-01-15 10:21:37 +0300

    implement collisions handling with linear probing

---
+++ structures/word_count.py
@@ -0,0 +1,16 @@
+with open('poem.txt', 'r') as f:
+    for line in f:
+        print(line)
+
+word_count = {}
+with open('poem.txt', 'r') as f:
+    for line in f:
+        tokens = line.split(' ')
+        for token in tokens:
+            token = token.replace('\n','')
+            if token in word_count:
+                word_count[token]+=1
+            else:
+                word_count[token]=1
+
+print(word_count)
