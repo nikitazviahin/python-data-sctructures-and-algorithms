d03e6f5807a5f9f0d929a4fe8e5f6ac2f777eccd
nikitazviahin <mykytazviahin@yahoo.com>
2021-01-19 11:22:18 +0300

    improve binary search

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -35,6 +35,9 @@ def binary_search_recursive(numbers_list, number_to_find, left_index, right_inde
         return -1
     
     mid_index = (left_index + right_index) // 2
+    if mid_index >= len(numbers_list) or mid_index < 0:
+        return -1
+
     mid_number = numbers_list[mid_index]
 
     if mid_number == number_to_find:
@@ -46,18 +49,42 @@ def binary_search_recursive(numbers_list, number_to_find, left_index, right_inde
         right_index = mid_index - 1
 
     return binary_search_recursive(numbers_list, number_to_find, left_index, right_index)
-        
+
+def find_all_occurances(numbers, number_to_find):
+    index = binary_search(numbers, number_to_find)
+    indices = [index]
+
+    i = index-1
+    while i >= 0:
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i -= 1
+
+    i = index+1
+    while i < len(numbers):
+        if numbers[i] == number_to_find:
+            indices.append(i)
+        else:
+            break
+        i += 1
+
+    return sorted(indices)
+           
 
 if __name__ == "__main__":
     # numbers_list = [12,5,6,11,4,567,123,45]
     # number_to_find = 12
 
-    numbers_list = [i for i in range(10000001)]
-    number_to_find = 123123
+    numbers_list = [1,2,3,4,5,5,5]
+    number_to_find = 5
 
     index_lin = linear_search(numbers_list, number_to_find) #~450 miliseconds
     index_bin = binary_search(numbers_list, number_to_find) #0.01 milisecond
     index_bin_rec = binary_search_recursive(numbers_list, number_to_find, 0, len(numbers_list)) 
+    indeces = find_all_occurances(numbers_list, number_to_find)
     print(f"Number found at {index_lin} using linear search")
     print(f"Number found at {index_bin} using binary search")
     print(f"Number found at {index_bin_rec} using binary search")
+    print(f"Number found at {indeces}")
